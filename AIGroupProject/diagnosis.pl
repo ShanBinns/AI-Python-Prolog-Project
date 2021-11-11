df0a9850d6a9cc960cce6b8acdcd8556ad2cc8b0
ShanBinns <shanbinns23@gmail.com>
2021-11-11 15:12:42 -0500

    Updated prolog and python files

--- AIGroupProject/diagnosis.pl
+++ AIGroupProject/diagnosis.pl
@@ -1,16 +1,68 @@
+/*   get_symptom(timothy,40.0,70,1,1,0,0,0,1,135,80).
+f = open("demofile3.txt", "w")
+
+  f.write("Woops! I have deleted the content!")
+
+f.close()
 
 
-get_symptom(Name,Temp,Dizziness,Fainting,Vision):-
-           (Temperature is Temp * 9/5 + 32),
-		   
-            nl,write("****************DIAGNOSIS*****************"),nl,
-			
-            write(Temperature),nl,write(Name),nl,
-			(Temperature > 100 -> Tempval is 1; Tempval is 0),
+
+  #open and read the file after the appending:
+
+f = open("demofile3.txt", "r")
+
+  print(f.read())
+*/
+
+get_symptom(Name,Temp,Age,Dizziness,Fainting,Vision,Coughing,Short_Breath,History,Systolic,Diastolic):-
+
+
+            nl,write("**************DIAGNOSIS***************"),nl,
+
+		   (Temperature is Temp * 9/5 + 32),
+           (Temperature > 100 -> Tempval is 1; Tempval is 0),
+           (Age > 65 -> Ageval is 1; Ageval is 0),
+
+		   write(Temp),nl,write(Name),nl,write(Age),nl,
+
+			%commonsymptoms
            (Dizziness is 1 ->  Dizzyvalue is 1;Dizzyvalue is 0),
            (Fainting is 1 -> Faintvalue is 1;Faintvalue is 0),
            (Vision is 1 -> Visionvalue is 1;Visionvalue is 0),
+		   (Coughing is 1 ->  Coughingvalue is 1;Coughingvalue is 0),
 
-			Riskvalue is Tempval+Dizzyvalue+Faintvalue+Visionvalue,
-			(Riskvalue > 2 -> nl,write("Patient: "),write(Name),write(" is at risk"),nl;
-			nl,write("Patient "),write(Name),write(" is not at risk"),nl).
+		   %SeriousSymptoms
+           (Short_Breath is 1 -> Breathingvalue is 1;Breathingvalue is 0),
+
+		   %UnderlyingIllness
+		   (History is 1 -> HighRiskvalue is 3;HighRiskvalue is 0),
+
+		   %Bloodpressure
+            blood_pressure(Systolic,Diastolic,BPrisk,BPcom),
+		   assess_risk(Name,Tempval,Ageval,Dizzyvalue,Faintvalue,Visionvalue,Coughingvalue,Breathingvalue,HighRiskvalue),
+		   recommendations(Name,Temperature,Age,BPrisk,BPcom).
+
+
+%Fix_this_function
+blood_pressure(Systolic,Diastolic,BPrisk,BPcom):-nl,BPrisk is 0,BPcom is 0,
+           (Systolic >= 120),(Systolic < 130),(Diastolic < 80) -> BPrisk is 1, BPcom = "elevated BP", 
+           ((Systolic >= 130),(Systolic < 140)); ((Diastolic >= 80),(Diastolic < 90) ) -> BPrisk is 2,BPcom = "stage 1 hypertension".
+       /*    ((Systolic>=140); (Diastolic>=90) )-> BPrisk is 3,BPcom = "stage 2 hypertension", write(BPrisk),write(BPcom).*/
+
+
+
+
+assess_risk(Name,Tempval,Ageval,Dizzyvalue,Faintvalue,Visionvalue,Coughingvalue,Breathingvalue,HighRiskvalue):-
+			Riskvalue is Tempval + Dizzyvalue + Faintvalue + Visionvalue + HighRiskvalue + Ageval + Coughingvalue +Breathingvalue,
+
+			(Riskvalue > 2 -> nl,write("Final Diagnosis: "),write(Name),write(" is at risk"),nl;
+			nl,write("Final Diagnosis "),write(Name),write(" is not at risk"),nl).
+
+
+
+recommendations(Name,Temperature,Age,BPrisk,BPcom):- nl,write("****COMMENTS AND RECOMMENDATION*****"),nl,
+						  write("**Based on analysis, the following factors puts patient "),write(Name),write(" at risk**"),nl,nl,
+                         (Temperature > 100) -> write("A fever, with a high temperature value of "),write(Temperature),nl,
+                         (Age > 65) -> write("Age risk: "),write(Age),nl,
+						 (BPrisk > 0) -> write("Experiencing : "),write(BPcom).
+
--- AIGroupProject/main.py
+++ AIGroupProject/main.py
@@ -5,31 +5,54 @@ from tkinter import messagebox
 
 # pip install pyswip
 
+# https://www.bma.org.uk/media/3820/bma-covid-19-risk-assessment-tool-february-2021.pdf
+# https://alama.org.uk/covid-19-medical-risk-assessment/
+
+# Fix Prolog file
+
+# Open other windows
 
 
 prolog = Prolog()
 prolog.consult("diagnosis.pl")
 
 
-class DiagnosisWindow:
+class add_covid_fact:
     def __init__(self, root):
         self.root = root
-        self.lbl_head = Label(root, text="Medical Diagnosis Evaluation Form", bd=5, relief=GROOVE,
+        self.lbl_head = Label(root, text="Add Knowledge about the Covid-19 virus", bd=5, relief=GROOVE,
                               font=("times new Roman", 20, "bold"),
-                              bg="lavender", fg="blue")
+                              bg="gold", fg="blue")
         self.lbl_head.pack(side=TOP, fill=X)
 
-        self.lbl1 = Label(root, text="Please fill in all field below with the patient's information", fg="blue",
-                          font=("times new Roman", 14, "bold"))
-        self.lbl1.place(x=150, y=50)
+        self.covid_factLabel = Label(root, text="Enter Covid-19 Fact:", fg="blue",font=("times new Roman", 14, "bold"))
+        self.covid_factLabel.place(x=10, y=100)
+        self.covid_factEntry = Entry(root, bd=3, width=30)
+        self.covid_factEntry.place(x=210, y=100)
+
+        #Submit Covid fact button
+        self.covid_factBtn = Button(root, text="Submit Fact", borderwidth=3, relief="sunken", command=(),
+                                width=15, height=2, bg="gold", fg="medium blue", font=("times new Roman", 10, "bold"))
+        self.covid_factBtn.place(x=250, y=200)
 
 
+class add_mu_fact:
+    def __init__(self, root):
+        self.root = root
+        self.lbl_head = Label(root, text="Add Knowledge about the Mu Variant", bd=5, relief=GROOVE,
+                              font=("times new Roman", 20, "bold"),
+                              bg="gold", fg="blue")
+        self.lbl_head.pack(side=TOP, fill=X)
 
+        self.mu_factLabel = Label(root, text="Enter Mu Variant Fact:", fg="blue",font=("times new Roman", 14, "bold"))
+        self.mu_factLabel.place(x=10, y=100)
+        self.mu_factEntry = Entry(root, bd=3, width=30)
+        self.mu_factEntry.place(x=250, y=100)
 
-
-
-
-
+        #Submit Covid fact button
+        self.mu_factBtn = Button(root, text="Submit Fact", borderwidth=3, relief="sunken", command=(),
+                                width=15, height=2, bg="gold", fg="medium blue", font=("times new Roman", 10, "bold"))
+        self.mu_factBtn.place(x=250, y=200)
 
 # Main window
 class MainWindow:
@@ -42,80 +65,45 @@ class MainWindow:
                       font=("times new Roman", 40, "bold"), bg="gold", fg="blue")
         title.pack(side=TOP, fill=X)
 
-        #Add Fact Buttons
-        self.covidFactBtn = Button(self.root,text="Add Covid-19 Fact",width=20,height=2,command=print(),
-                                   bg="blue", fg="gold",font=("times new Roman", 12, "bold"))
+        # Add Fact Buttons
+        self.covidFactBtn = Button(self.root, text="Add Covid-19 Fact", width=20, height=2, command=self.add_covid_fact,
+                                   bg="medium blue", fg="gold", font=("times new Roman", 12, "bold"))
         self.covidFactBtn.place(x=300, y=80)
 
-        self.muFactBtn = Button(self.root, text="Add Mu Variant Fact", width=20, height=2, command=print(),
-                                bg="blue", fg="gold",font=("times new Roman", 12, "bold"))
+        self.muFactBtn = Button(self.root, text="Add Mu Variant Fact", width=20, height=2, command=self.add_mu_fact,
+                                bg="medium blue", fg="gold", font=("times new Roman", 12, "bold"))
         self.muFactBtn.place(x=600, y=80)
 
         self.deltaFactBtn = Button(self.root, text="Add Delta Variant Fact", width=20, height=2, command=print(),
-                                   bg="blue", fg="gold",font=("times new Roman", 12, "bold"))
+                                   bg="medium blue", fg="gold", font=("times new Roman", 12, "bold"))
         self.deltaFactBtn.place(x=900, y=80)
 
-        self.lbl1 = Label(self.root, text="Please fill in all field below with the patient's information", fg="blue",
-                          font=("times new Roman", 14, "bold"))
-        self.lbl1.place(x=400, y=160)
+        self.lbl1 = Label(self.root, text="Use the quick link buttons to add facts or fill out the form below to "
+                                          "diagnosis patient", fg="blue", font=("times new Roman", 14, "bold"))
+        self.lbl1.place(x=200, y=160)
 
         # Section One Frame
         detail_frame = Frame(root, bd=4, relief=RIDGE)
         detail_frame.place(x=20, y=200, width=500, height=450)
 
-        # RadioButton Variables
-        self.var1 = IntVar()
-        self.var2 = IntVar()
-        self.var3 = IntVar()
-
-        self.lbl_search = Label(detail_frame, text="Section 1: Demographics and Symptoms", fg="blue",
+        self.lbl_search = Label(detail_frame, text="Section 1: Patient Demographics and Health History", fg="blue",
                                 font=("times new Roman", 15, "bold"))
         self.lbl_search.place(x=10, y=10)
 
-        self.lbl1 = Label(detail_frame, text="Name:", fg="blue", font=("times new Roman", 12, "bold"))
+        self.lbl1 = Label(detail_frame, text="Name:", font=("times new Roman", 12))
         self.lbl1.place(x=10, y=50)
-        self.name = Entry(detail_frame, bd=3, width=30)
-        self.name.place(x=300, y=50)
+        self.name = Entry(detail_frame, bd=3, width=25)
+        self.name.place(x=200, y=50)
 
-        self.lbl2 = Label(detail_frame, text="Age:", fg="blue",
-                          font=("times new Roman", 12, "bold"))
+        self.lbl2 = Label(detail_frame, text="Age:", font=("times new Roman", 12))
         self.lbl2.place(x=10, y=92)
-        self.temp = Entry(detail_frame, bd=3, width=10)
-        self.temp.place(x=300, y=90)
+        self.age = Entry(detail_frame, bd=3, width=10)
+        self.age.place(x=200, y=90)
 
-        self.lbl3 = Label(detail_frame, text="Weight (in pounds):", fg="blue",
-                          font=("times new Roman", 12, "bold"))
+        self.lbl3 = Label(detail_frame, text=" Temperature (in degrees):", font=("times new Roman", 12))
         self.lbl3.place(x=10, y=130)
         self.temp = Entry(detail_frame, bd=3, width=10)
-        self.temp.place(x=300, y=130)
-
-        self.lbl3 = Label(detail_frame, text=" Temperature (in degrees):", fg="blue",
-                          font=("times new Roman", 12, "bold"))
-        self.lbl3.place(x=10, y=170)
-        self.temp = Entry(detail_frame, bd=3, width=10)
-        self.temp.place(x=300, y=170)
-
-        self.lbl = Label(detail_frame, text="The patient is experiencing Dizziness:", fg="blue",
-                         font=("times new Roman", 12, "bold"))
-        self.lbl.place(x=10, y=210)
-        Radiobutton(detail_frame, text="yes", variable=self.var1, value=1).place(x=300, y=210)
-        Radiobutton(detail_frame, text="no", variable=self.var1, value=0).place(x=370,y=210)
-
-        self.lbl = Label(detail_frame, text="The patient is experiencing Fainting:", fg="blue",
-                         font=("times new Roman", 12, "bold"))
-        self.lbl.place(x=10, y=240)
-        Radiobutton(detail_frame, text="yes", variable=self.var2, value=1).place(x=300, y=240)
-        Radiobutton(detail_frame, text="no", variable=self.var2, value=0).place(x=370, y=240)
-
-        self.lbl = Label(detail_frame, text="The patient is experiencing Blurred Vision:", fg="blue",
-                         font=("times new Roman", 12, "bold"))
-        self.lbl.place(x=10, y=280)
-        Radiobutton(detail_frame, text="yes", variable=self.var3, value=1).place(x=300, y=280)
-        Radiobutton(detail_frame, text="no", variable=self.var3, value=0).place(x=370, y=280)
-
-        #Patient history frame
-        history_frame = Frame(root, bd=4, relief=RIDGE)
-        history_frame.place(x=570, y=200, width=500, height=450)
+        self.temp.place(x=200, y=130)
 
         # Checkbox Variables
         self.var_check1 = IntVar()
@@ -125,76 +113,164 @@ class MainWindow:
         self.var_check5 = IntVar()
         self.var_check6 = IntVar()
         self.var_check7 = IntVar()
+        self.var_check8 = IntVar()
+
+        self.lbl = Label(detail_frame, text="Select all underlying illnesses that applies patient.", fg="blue",
+                         font=("times new Roman", 12, "bold"))
+        self.lbl.place(x=10, y=200)
+
+        Checkbutton(detail_frame, text="Heart Disease", variable=self.var_check1, onvalue=1, offvalue=0).place(x=10,
+                                                                                                               y=240)
+        Checkbutton(detail_frame, text="Lung Conditions (including severe asthma,pneumonia,Cystic fibrosis)",
+                    variable=self.var_check2, onvalue=1, offvalue=0).place(x=10, y=260)
+        Checkbutton(detail_frame, text="Weakened Immune System (including Organ transplants, HIV/AIDs,",
+                    variable=self.var_check3, onvalue=1, offvalue=0).place(x=10, y=280)
+        Checkbutton(detail_frame, text="Diabetes", variable=self.var_check4, onvalue=1, offvalue=0).place(x=10, y=300)
+        Checkbutton(detail_frame, text="Hypertension", variable=self.var_check5, onvalue=1, offvalue=0).place(x=10,
+                                                                                                              y=320)
+        Checkbutton(detail_frame, text="Chronic kidney or liver disease", variable=self.var_check6, onvalue=1,
+                    offvalue=0).place(x=10, y=340)
+        Checkbutton(detail_frame, text="Down Syndrome", variable=self.var_check7, onvalue=1, offvalue=0).place(x=10,
+                                                                                                               y=360)
+        Checkbutton(detail_frame, text="Obesity", variable=self.var_check8, onvalue=1, offvalue=0).place(x=10, y=380)
+
+        # List to save checkbox responses
+        self.List = []
+        self.varList = []
+        self.varList.append(self.var_check1)
+        self.varList.append(self.var_check2)
+        self.varList.append(self.var_check3)
+        self.varList.append(self.var_check4)
+        self.varList.append(self.var_check5)
+        self.varList.append(self.var_check6)
+        self.varList.append(self.var_check7)
+        self.varList.append(self.var_check8)
+
+        # Patient history frame
+        history_frame = Frame(root, bd=4, relief=RIDGE)
+        history_frame.place(x=570, y=200, width=500, height=450)
 
         self.lbl_search = Label(history_frame, text="Section 2: Patient Medical History", fg="blue",
                                 font=("times new Roman", 15, "bold"))
         self.lbl_search.place(x=10, y=10)
 
-        self.lbl = Label(history_frame, text="Select all that applies.", fg="blue", font=("times new Roman", 12))
+        self.lbl = Label(history_frame, text="Is the patient experiencing any of the following symptoms?", fg="blue",
+                         font=("times new Roman", 12, "bold"))
         self.lbl.place(x=10, y=50)
 
-        Checkbutton(history_frame, text="Heart Disease", variable=self.var_check1,onvalue=1,offvalue=0).place(x=10,y=80)
-        Checkbutton(history_frame, text="Lung Conditions (including severe asthma,pneumonia,Cystic fibrosis)",
-                    variable=self.var_check2, onvalue=1, offvalue=0).place(x=10, y=110)
-        Checkbutton(history_frame, text="Weakened Immune System (including Organ transplants, HIV/AIDs,",
-                    variable=self.var_check3, onvalue=1, offvalue=0).place(x=10, y=140)
-        Checkbutton(history_frame, text="Diabetes", variable=self.var_check4, onvalue=1, offvalue=0).place(x=10, y=170)
-        Checkbutton(history_frame, text="Hypertension",variable=self.var_check5,onvalue=1,offvalue=0).place(x=10,y=200)
-        Checkbutton(history_frame, text="Chronic kidney or liver disease", variable=self.var_check6, onvalue=1,
-                    offvalue=0).place(x=10, y=230)
-        Checkbutton(history_frame, text="Down Syndrome",variable=self.var_check7,onvalue=1,offvalue=0).place(x=10,y=260)
+        # RadioButton Variables
+        self.vardiz = IntVar()
+        self.varfai = IntVar()
+        self.varvis = IntVar()
+        self.varcou = IntVar()
+        self.varsho = IntVar()
+
+        self.lbl = Label(history_frame, text="Dizziness:", font=("times new Roman", 12))
+        self.lbl.place(x=10, y=90)
+        Radiobutton(history_frame, text="yes", variable=self.vardiz, value=1).place(x=200, y=90)
+        Radiobutton(history_frame, text="no", variable=self.vardiz, value=0).place(x=270, y=90)
+
+        self.lbl = Label(history_frame, text="Fainting:", font=("times new Roman", 12))
+        self.lbl.place(x=10, y=120)
+        Radiobutton(history_frame, text="yes", variable=self.varfai, value=1).place(x=200, y=120)
+        Radiobutton(history_frame, text="no", variable=self.varfai, value=0).place(x=270, y=120)
+
+        self.lbl = Label(history_frame, text="Blurred Vision:", font=("times new Roman", 12))
+        self.lbl.place(x=10, y=150)
+        Radiobutton(history_frame, text="yes", variable=self.varvis, value=1).place(x=200, y=150)
+        Radiobutton(history_frame, text="no", variable=self.varvis, value=0).place(x=270, y=150)
+
+        self.lbl = Label(history_frame, text="Cough:", font=("times new Roman", 12))
+        self.lbl.place(x=10, y=180)
+        Radiobutton(history_frame, text="yes", variable=self.varcou, value=1).place(x=200, y=180)
+        Radiobutton(history_frame, text="no", variable=self.varcou, value=0).place(x=270, y=180)
+
+        self.lbl = Label(history_frame, text="Shortness of Breath:", font=("times new Roman", 12))
+        self.lbl.place(x=10, y=210)
+        Radiobutton(history_frame, text="yes", variable=self.varsho, value=1).place(x=200, y=210)
+        Radiobutton(history_frame, text="no", variable=self.varsho, value=0).place(x=270, y=210)
+
+        # Blood Pressure
+        self.lbl = Label(history_frame, text="Please enter blood pressure reading", fg="blue",
+                         font=("times new Roman", 12, "bold"))
+        self.lbl.place(x=10, y=250)
+
+        self.lbl2 = Label(history_frame, text="Systolic:", font=("times new Roman", 12))
+        self.lbl2.place(x=10, y=280)
+        self.systolic = Entry(history_frame, bd=3, width=10)
+        self.systolic.place(x=200, y=280)
+
+        self.lbl2 = Label(history_frame, text="Diastolic:", font=("times new Roman", 12))
+        self.lbl2.place(x=10, y=310)
+        self.diastolic = Entry(history_frame, bd=3, width=10)
+        self.diastolic.place(x=200, y=310)
 
         # Submit button
-        self.submitBtn = Button(root, text="submit",command=self.submit,width=30,height=2, bg="blue", fg="gold",font=("times new Roman", 12, "bold"))
-        self.submitBtn.place(x=1100, y=600)
+        self.submitBtn = Button(root, text="Diagnose Patient", borderwidth=3, relief="sunken", command=self.submit,
+                                width=20, height=2, bg="gold", fg="medium blue", font=("times new Roman", 12, "bold"))
+        self.submitBtn.place(x=1090, y=600)
 
     # Function that queries prolog file
     def submit(self):
         name_var = self.name.get()
-      #  temp_var = float(self.temp.get())
-        dizzy_choice = int(self.var1.get())
-        faint_choice = int(self.var2.get())
-        vision_choice = int(self.var3.get())
+        temp_var = float(self.temp.get())
+        age_var = int(self.age.get())
+
         history_var = IntVar()
 
+        dizzy_choice = int(self.vardiz.get())
+        faint_choice = int(self.varfai.get())
+        vision_choice = int(self.varvis.get())
+        cough_choice = int(self.varcou.get())
+        shortbreath_choice = int(self.varsho.get())
 
+        systolic_var = int(self.systolic.get())
+        diastolic_var = int(self.diastolic.get())
+        print(systolic_var)
+        print(diastolic_var)
+
+        # Checkbox
         def isChecked():
-            for c in (self.var_check1.get(),self.var_check2.get(),self.var_check3.get(),self.var_check4.get(),
-                      self.var_check4.get(),self.var_check6.get(),self.var_check7.get()):
-               if c==1:
-                   break
-                   history_var = 1
-                   print(history_var)
-                   break
-               else:
-                   break
-                   history_var = 0
-                   print(history_var)
+            global List
 
-
-
-
+            List = []
+            for item in self.varList:
+                if item.get() != "":
+                    List.append(item.get())
 
         isChecked()
-        print(history_var)
+        # print(List)
+        if 1 in List:
+            history_var = 1
+        else:
+            history_var = 0
 
+        result = list(prolog.query("get_symptom(%s,%f,%d,%d,%d,%d,%d,%d,%d,%d,%d)."
+                                   % (
+                                       name_var, temp_var, age_var, dizzy_choice, faint_choice, vision_choice,
+                                       cough_choice, shortbreath_choice, history_var, systolic_var, diastolic_var)))
 
-
-        #result = list(prolog.query("get_symptom(%s, %f, %d,%d,%d)." % (name_var, temp_var, dizzy_choice, faint_choice,
-             #                                                          vision_choice)))
-
-        # return prolog.query("get_symptom(%d,%d,%d)." % (dizzy_choice,faint_choice,vision_choice))
         # write the result to file in prolog, then read from file to messagebox
         # tkinter.messagebox.showinfo("Your results", result)
 
-    def diagnosis_window(self):
-        diagnosis = Tk()
-        diag = DiagnosisWindow(diagnosis)
-        diagnosis.title("Diagnosis Patient")
-        diagnosis.geometry("1200x1000+10+10")
-        diagnosis.mainloop()
+    #functions for add fact buttons
+    def add_covid_fact(self):
+        covid_fact = Tk()
+        cfact = add_covid_fact(covid_fact)
+        covid_fact.title("Add Covid Fact")
+        covid_fact.geometry("600x400+10+10")
+        covid_fact.mainloop()
+
+    def add_mu_fact(self):
+        mu_fact = Tk()
+        mfact = add_mu_fact(mu_fact)
+        mu_fact.title("Add Mu Variant Fact")
+        mu_fact.geometry("600x400+10+10")
+        mu_fact.mainloop()
 
 
 root = Tk()
 ob = MainWindow(root)
 root.mainloop()
+
+
