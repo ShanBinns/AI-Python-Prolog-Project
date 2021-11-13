eee834fcd606db00de379e4994e8db368a22fac2
Patrick <iipajcii@gmail.com>
2021-11-13 16:03:44 -0500

    commit patient.pl file to project
    This is where patient information will be stored

---
+++ AIGroupProject/illnesses.pl
@@ -0,0 +1,7 @@
+illness(normal, lung_condition, 1, 0).
+illness(normal, weakened_immune_system, 1, 0).
+illness(normal, diabetes, 1, 0).
+illness(normal, hypertension, 1, 0).
+illness(normal, chronic_kidney_or_liver_disease, 1, 0).
+illness(normal, down_syndrome, 1, 0).
+illness(normal, obesity, 1, 0).
